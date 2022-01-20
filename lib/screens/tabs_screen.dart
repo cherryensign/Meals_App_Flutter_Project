aac51aeb8d671bf54d04d94d07d8314860accfa4
cherryensign <mithileshghadge44@gmail.com>
2022-01-20 20:21:38 +0530

    Added Drawer

--- lib/screens/tabs_screen.dart
+++ lib/screens/tabs_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import './favorite_screen.dart';
 import '../screens/categories_screen.dart';
+import '../widgets/main_drawer.dart';
 
 class TabScreen extends StatefulWidget {
   @override
@@ -32,6 +33,7 @@ class _TabScreenState extends State<TabScreen> {
       appBar: AppBar(
         title: Text(_pages[_selectedPageIndex]['title']),
       ),
+      drawer: MainDrawer(),
       body: _pages[_selectedPageIndex]['page'],
       bottomNavigationBar: BottomNavigationBar(
         onTap: _selectPage,
---
+++ lib/widgets/main_drawer.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+class MainDrawer extends StatelessWidget {
+  Widget buildListTile(String title, IconData icon) {
+    return ListTile(
+      leading: Icon(
+        icon,
+        size: 26,
+      ),
+      title: Text(
+        title,
+        style: TextStyle(
+          fontFamily: 'RobotoCondensed',
+          fontSize: 24,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+      onTap: () {},
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: Column(
+        children: [
+          Container(
+            height: 150,
+            width: double.infinity,
+            padding: EdgeInsets.all(20),
+            alignment: Alignment.bottomLeft,
+            color: Theme.of(context).accentColor,
+            child: Text(
+              'Cooking up!',
+              style: TextStyle(
+                fontWeight: FontWeight.w900,
+                fontSize: 30,
+                color: Theme.of(context).primaryColor,
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          buildListTile('Meals', Icons.restaurant),
+          buildListTile('Filters', Icons.settings),
+        ],
+      ),
+    );
+  }
+}
